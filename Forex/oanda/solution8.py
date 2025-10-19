6225712253dce66e02c709f681c533fc1e90a8a8
Excaliabure <106698782+Excaliabure@users.noreply.github.com>
2025-10-18 22:54:50 -0700

    YAY SERVER TIME

--- Forex/oanda/solution8.py
+++ Forex/oanda/solution8.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.integrate import solve_ivp
 # from scipy.integrate import odeint
 # import torch.nn as nn
 
@@ -6,18 +7,37 @@ import numpy as np
 n = 2 # bodies 
 
 G = 1
-m = np.array([100, 1])
-x = np.array([[1,2], # x1
-     [2,1] # x2
-     ])
 
-a = np.zeros(n)
+# Mass1, x1, y1, z1, Mass2, x2, y2, z2, ...
 
-print("Calculating A..")
-for i in range(n):
-    for j in range(n):
-        if j != i:
-            a[i] = G * sum(m[i] * (x[j] - x[i]) / np.pow(np.linalg.norm(x[j] - x[i]),3))
+# Note only doing positions
+ic = np.array([
+     ]) 
+     
+
+def f(t, y, dims=3):
+    # Central Body at 0,0,0 and mass 100 
+    
+    
+    pos = y[:n*dims].reshape(n, dims)
+    vel  = y[:n*dims].reshape(n, dims)
+    acc = np.zeros(vel.shape[0])
+    print(vel.shape)
+    a = np.zeros(len(masses))
+
+    for i in range(len(masses)):
+        for j in range(len(masses)):
+            if j != i:
+                r = pos[j] - pos[i]
+                acc[i] =  (masses[i] *  r / np.pow(np.linalg.norm(r), 3)).sum()
+                
+    
+    return a
 
 
-print(a)
+t_span = (0,20)
+dt = 20
+
+# print(solve_ivp(f,t_span=t_span, y0=ic))
+print(f(1,ic))
+
